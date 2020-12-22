111be0c57b8e477eed455ecaa990202e6814ff38
DalitsoKasonde <dkasonde11@gmail.com>
2020-12-22 02:04:03 +0200

    state

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,32 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  int LeftDiceNum = 4;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    LeftDiceNum = 2;
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed');
+              },
+              child: Image.asset('images/dice$LeftDiceNum.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
